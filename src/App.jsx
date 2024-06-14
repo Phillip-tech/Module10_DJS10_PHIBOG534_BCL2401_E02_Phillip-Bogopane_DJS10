d5d541051384475c050f2c5c280b36688e9a1588
Phillip Bogopane <pm.bogopane@gamil.com>
2024-06-14 14:15:33 +0200

    Coded codethat is part of an asynchronous function that fetches data from an API.

--- src/App.jsx
+++ src/App.jsx
@@ -6,3 +6,21 @@ const App = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts?lang=en');
+        if (!response.ok) {
+          throw new Error('Failed to fetch');
+        
+          const data = await response.json();
+          setPosts(data);
+        } catch (error) {
+          setError('Data fetching failed');
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+  
